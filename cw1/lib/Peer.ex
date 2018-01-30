9cdeaa9b0eebaf59c7937669f6586ad5216a0f81
Matthew Brookes <mbrookes1304@gmail.com>
2018-01-30 16:59:04 +0000

    Pass peers their id and peers

--- cw1/lib/Peer.ex
+++ cw1/lib/Peer.ex
@@ -1,6 +1,12 @@
 # Matthew Brookes (mb5715) and Abhinav Mishra (am8315)
 defmodule Peer do
-    def start do
-      IO.puts "Started"
+    def start() do
+      receive do
+        { id, peers } -> next(id, peers)
+      end
+    end
+
+    defp next(id, peers) do
+      IO.puts(id)
     end
 end
--- cw1/lib/System1.ex
+++ cw1/lib/System1.ex
@@ -1,11 +1,12 @@
 # Matthew Brookes (mb5715) and Abhinav Mishra (am8315)
 defmodule System1 do
   def main do
-    peers = for _ <- 1..5, do: spawn(Peer, :start, [])
     max_broadcasts = 1000
     timeout = 3000
-    for peer <- peers, do:
-      send peer, {:broadcast, max_broadcasts, timeout}
+    num_peers = 5
+    peers = for _ <- 1..num_peers, do: spawn(Peer, :start, [])
+    for i <- 1..num_peers, do:
+      send Enum.at(peers, i - 1), {i, peers}
     # Send message to first peer
     send Enum.at(peers, 0), {:broadcast, 1000, 3000}
   end
