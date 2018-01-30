23ddaf448c5ca27be2dbc18a8f6812a5e082f394
Matthew Brookes <mbrookes1304@gmail.com>
2018-01-30 18:10:22 +0000

    Send messages until limit

--- cw1/lib/Peer.ex
+++ cw1/lib/Peer.ex
@@ -13,19 +13,32 @@ defmodule Peer do
   end
 
   defp broadcast(id, peers, max_messages, timeout, sent_count, sent_map, receive_map) do
-    new_sent_count = sent_count
-    for i <- 1..length(peers) do
-      Map.update(sent_map, i, 1, &(&1 + 1))
-      new_sent_count = new_sent_count + 1
-      send Enum.at(peers, i - 1), { :hello, id }
-    end
-
-    receive do
-      { :hello, id } ->
-        Map.update(receive_map, id, 1, &(&1 + 1))
-        broadcast(id, peers, max_messages, timeout, new_sent_count, sent_map, receive_map)
-    after timeout ->
-      IO.puts [id, " timed out"]
+    if sent_count == 0 do
+      for i <- 1..length(peers) do
+        Map.update(sent_map, i, 1, &(&1 + 1))
+        send Enum.at(peers, i - 1), { :hello, id }
+      end
+      broadcast(id, peers, max_messages, timeout, sent_count + length(peers), sent_map, receive_map)
+    else
+      if sent_count < max_messages do
+        receive do
+          { :hello, receive_id } ->
+            Map.update(receive_map, receive_id, 1, &(&1 + 1))
+            for i <- 1..length(peers) do
+              Map.update(sent_map, i, 1, &(&1 + 1))
+              send Enum.at(peers, i - 1), { :hello, id }
+            end
+            broadcast(id, peers, max_messages, timeout, sent_count + length(peers), sent_map, receive_map)
+        after timeout ->
+          IO.puts [id, " timed out"]
+        end
+      else
+        output = "#{id}: "
+        for i <- 1..length(peers) do
+          output = output <> "{#{sent_map[id]}:#{receive_map[Enum.at(peers, i - 1)]}} "
+        end
+        IO.puts output
+      end
     end
   end
 end
