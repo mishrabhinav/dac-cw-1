c62c3f013df42fd22b1444ae4b180023080b442b
Matthew Brookes <mbrookes1304@gmail.com>
2018-01-30 16:21:16 +0000

    Initial mix project

---
+++ cw1/lib/cw1.ex
@@ -0,0 +1,18 @@
+defmodule Cw1 do
+  @moduledoc """
+  Documentation for Cw1.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Cw1.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ cw1/test/cw1_test.exs
@@ -0,0 +1,8 @@
+defmodule Cw1Test do
+  use ExUnit.Case
+  doctest Cw1
+
+  test "greets the world" do
+    assert Cw1.hello() == :world
+  end
+end
