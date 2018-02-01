a4b9fb210075984364f5017154e3ccc849ce5d22
Matthew Brookes <mbrookes1304@gmail.com>
2018-02-01 15:44:11 +0000

    Remove Peer2

--- cw1/lib/Peer.ex
+++ cw1/lib/Peer.ex
@@ -12,31 +12,46 @@ defmodule Peer do
     receive do
       { :broadcast, max_messages, timeout } ->
         counter = for _ <- peers, do: {0, 0}
-        broadcast id, peers, max_messages, timeout, 0, counter
+        broadcast id, peers, max_messages, curr_time() + timeout, counter
     end
   end # next
 
-  defp broadcast id, peers, max_messages, timeout, sent_count, counter do
-    if sent_count < max_messages do
-      for i <- 1..length(peers), do:
-        send Enum.at(peers, i-1), { :hello, id }
-
-      counter = Enum.map(counter, fn {s, r} -> {s+1, r} end)
-    end
-
-    receive do
-      { :hello, recv_id } ->
-        counter = List.update_at(counter, recv_id, fn {s, r} -> {s, r+1} end)
-        broadcast id, peers, max_messages, timeout, sent_count + 1, counter
-    after
-      timeout ->
-        output = ["#{id+1}:"]
-
+  defp broadcast id, peers, messages, end_time, counter do
+    cond do
+      curr_time() >= end_time ->
         stats = for {sent, recv} <- counter, do: "{#{sent}:#{recv}}"
+        IO.puts Enum.join(["#{id}:"] ++ stats, " ")
 
-        IO.puts Enum.join(output ++ stats, " ")
+      messages > 0 ->
+        for i <- 1..length(peers), do:
+          send Enum.at(peers, i-1), { :hello, id }
+
+        counter = Enum.map(counter, fn {s, r} -> {s+1, r} end)
+
+        receive do
+          { :hello, recv_id } ->
+            counter = List.update_at(counter, recv_id, fn {s, r} -> {s, r+1} end)
+            broadcast id, peers, messages - 1, end_time, counter
+        after
+          0 ->
+            broadcast id, peers, messages , end_time, counter
+        end
+
+      true ->
+        receive do
+          { :hello, recv_id } ->
+            counter = List.update_at(counter, recv_id, fn {s, r} -> {s, r+1} end)
+            broadcast id, peers, messages - 1, end_time, counter
+        after
+          0 ->
+            broadcast id, peers, messages , end_time, counter
+        end
     end
 
   end # broadcast
+
+  defp curr_time do
+        :os.system_time(:millisecond)
+  end
 
 end # Peer
