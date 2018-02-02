24cb08148b85eec8c754d1a29cfa978d83068036
Abhinav Mishra <abhinav.mishra15@imperial.ac.uk>
2018-02-02 20:29:41 +0000

    Copy over System5

---
+++ System6/lib/app.ex
@@ -0,0 +1,56 @@
+# Matthew Brookes (mb5715) and Abhinav Mishra (am8315)
+defmodule App do
+
+  def start id do
+    receive do
+      {:bind, beb, num_peers } ->
+        wait_for_broadcast id, beb, num_peers
+    end
+  end # start
+
+  defp wait_for_broadcast id, beb, num_peers do
+    receive do
+      { :beb_deliver, max_messages, timeout } ->
+        counter = for _ <- 1..num_peers, do: {0, 0}
+        broadcast id, beb, max_messages, curr_time() + timeout, counter
+    end
+  end # wait_for_broadcast
+
+  defp broadcast id, beb, messages, end_time, counter do
+    cond do
+      curr_time() >= end_time ->
+        stats = for {sent, recv} <- counter, do: "{#{sent}:#{recv}}"
+        IO.puts Enum.join(["#{id}:"] ++ stats, " ")
+
+      messages > 0 ->
+        send beb, { :beb_broadcast, :hello }
+
+        counter = Enum.map(counter, fn {s, r} -> {s+1, r} end)
+
+        receive do
+          { :beb_deliver, from, :hello } ->
+            counter = List.update_at(counter, from - 1, fn {s, r} -> {s, r+1} end)
+            broadcast id, beb, messages - 1, end_time, counter
+        after
+          0 ->
+            broadcast id, beb, messages - 1, end_time, counter
+        end
+
+      true ->
+        receive do
+          { :beb_deliver, from, :hello } ->
+            counter = List.update_at(counter, from - 1, fn {s, r} -> {s, r+1} end)
+            broadcast id, beb, messages - 1, end_time, counter
+        after
+          0 ->
+            broadcast id, beb, messages, end_time, counter
+        end
+    end
+
+  end # broadcast
+
+  defp curr_time do
+        :os.system_time(:millisecond)
+  end # curr_time
+
+end # App
--- System6/lib/system6.ex
+++ System6/lib/system6.ex
@@ -1,18 +1,31 @@
 defmodule System6 do
-  @moduledoc """
-  Documentation for System6.
-  """
 
-  @doc """
-  Hello world.
+  def main do
+    max_broadcasts = 1000
+    timeout = 3000
+    kill_timeout = 5
+    num_peers = 5
+    lpl_drop_rate = 33
 
-  ## Examples
+    peers =
+      case System.get_env("DOCKER") || "false" do
+        "true" -> for i <- 1..num_peers, do: { i, Node.spawn(:"peer#{i}@peer#{i}.localdomain", Peer, :start, [i, self(), lpl_drop_rate, kill_timeout]) }
+        "false" -> for i <- 1..num_peers, do: { i, spawn(Peer, :start, [i, self(), lpl_drop_rate, kill_timeout]) }
+      end
 
-      iex> System6.hello
-      :world
+    peer_bebs =
+      for _ <- 1..num_peers do
+        receive do
+          { :response, id, lpl, beb, app } -> { id, lpl, beb, app }
+        end
+      end
 
-  """
-  def hello do
-    :world
-  end
+    for { _, peer } <- peers, do:
+      send peer, { :bind, peer_bebs }
+
+    for { _, lpl, _, _ } <- peer_bebs, do:
+      send lpl, { :pl_send, max_broadcasts, timeout }
+
+  end # main
+
 end
