933dd6262ff3d81c69afb9acc3c0c6e41f760d76
Matthew Brookes <mbrookes1304@gmail.com>
2018-02-05 16:08:47 +0000

    LRB: add sequence number to messages

--- System7/lib/lrb.ex
+++ System7/lib/lrb.ex
@@ -15,32 +15,32 @@ defmodule LRB do
       { :beb_deliver, max_broadcasts, timeout } ->
         send app, { :lrb_deliver, max_broadcasts, timeout }
         process_msgs = Map.new(processes, fn p -> {p, MapSet.new} end)
-        next id, beb, app, processes, process_msgs
+        next id, beb, app, processes, process_msgs, 0
     end
   end # wait_for_broadcast
 
-  defp next id, beb, app, correct, process_msgs do
+  defp next id, beb, app, correct, process_msgs, seq do
     receive do
       { :lrb_broadcast, message } ->
-        send beb, { :beb_broadcast, { :lrb_data, id, message } }
-        next id, beb, app, correct, process_msgs
+        send beb, { :beb_broadcast, { :lrb_data, id, message, seq } }
+        next id, beb, app, correct, process_msgs, seq+1
 
       { :fd_crash, { crashed_id, _} } ->
-        for m <- process_msgs[crashed_id], do:
-          send beb, { :beb_broadcast, { :lrb_data, crashed_id, m } }
-        next id, beb, app, MapSet.delete(correct, crashed_id), process_msgs
+        for { m, g_seq } <- process_msgs[crashed_id], do:
+          send beb, { :beb_broadcast, { :lrb_data, crashed_id, m, g_seq } }
+        next id, beb, app, MapSet.delete(correct, crashed_id), process_msgs, seq
 
-      { :beb_deliver, _, { :rb_data, sender, m } = lrb_m } ->
-        if MapSet.member?(process_msgs[sender], m) do
-          next id, beb, app, correct, process_msgs
+      { :beb_deliver, _, { :lrb_data, sender, m, g_seq } = lrb_m } ->
+        if MapSet.member?(process_msgs[sender], { m, g_seq }) do
+          next id, beb, app, correct, process_msgs, seq
         else
           send app, { :lrb_deliver, sender, m }
-          sender_msgs = MapSet.put process_msgs[sender], m
+          sender_msgs = MapSet.put process_msgs[sender], { m, g_seq }
           process_msgs = Map.put process_msgs, sender, sender_msgs
           unless Enum.member? correct, sender do
             send beb, { :beb_broadcast, lrb_m }
           end
-          next id, beb, app, correct, process_msgs
+          next id, beb, app, correct, process_msgs, seq
         end
     end
   end # next
